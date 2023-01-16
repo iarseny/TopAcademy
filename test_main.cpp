f7b0b5a61da50a9578db05ba43972c8649045433
Dennet Cordelia <36990341+RouDenr@users.noreply.github.com>
2023-01-16 15:36:08 +0000

    header file; plus func

--- test_main.cpp
+++ test_main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
+#include "main.hpp"
 
 int main(int argc, char const *argv[]) {
-    std::cout << "Hello world!\n";
+    std::cout << "Hello world! " << plus(5, 3) <<" \n";
     return 0;
 }
